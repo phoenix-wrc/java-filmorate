4bc7a557ca4adcded681c5c6246519e77fc149cf
Novozhilov Cyrill <phoenix-wrc@mail.ru>
2022-10-22 15:13:39 +1000

    Поправил и добавил функциональность в ФилмСервис. Все тесты теперь проходятся. Добавил ошибку удаления лайка и не находа фильма. Исправил сортировку возвращаемого списка популярных фильмов.

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
@@ -0,0 +1,77 @@
+package ru.yandex.practicum.filmorate.service.film;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class FilmService {
+	private final FilmStorage storage;
+
+	@Autowired
+	public FilmService(FilmStorage storage) {
+		this.storage = storage;
+	}
+
+	public boolean addLikeToFilm(Integer idFilm, Integer userId) {
+		Film f = storage.getFilm(idFilm);
+		return f.addLike(userId);
+	}
+
+	public boolean removeLikeFromFilm(Integer idFilm, Integer userId) {
+		Film f = storage.getFilm(idFilm);
+		return f.removeLike(userId);
+	}
+
+	public List<Film> getTopFilms(Integer count) {
+		List<Film> out;
+		if(count > 0) {
+			out = storage.films().stream().sorted(Comparator.comparing(
+					Film::getCountOfLikes).reversed())
+					.limit(count).
+					collect(Collectors.toList());
+		} else {
+			throw new ValidationException("Параметр должен быть больше ноля");
+		}
+		return out;
+	}
+
+	public Film add(Film film) {
+		return storage.add(film);
+	}
+
+	public Film delete(Integer id) {
+		return storage.delete(id);
+	}
+
+	public Film patch(Film film) {
+		return storage.patch(film);
+	}
+
+	public List<Film> films() {
+		return new ArrayList<>(storage.films());
+	}
+
+	public Integer size() {
+		return storage.size();
+	}
+
+	public Film getFilm(Integer id) {
+		return storage.getFilm(id);
+	}
+
+//	будет отвечать за операции с фильмами, —
+//		добавление и удаление лайка,
+//		вывод 10 наиболее популярных фильмов по количеству лайков.
+//
+//	Пусть пока каждый пользователь может поставить лайк фильму только один раз.
+
+}
